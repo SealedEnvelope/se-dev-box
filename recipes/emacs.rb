c1446cf5f08ed5cb809a63d3aa4fa0b062796b8e
Jon Ramsey <jonathon.ramsey@gmail.com>
2013-03-14 16:02:38 +0000

    Removing unnecessary dmg_name for Emacs.

--- recipes/emacs.rb
+++ recipes/emacs.rb
@@ -10,7 +10,6 @@ end
 
 dmg_package "Emacs" do
   app "Emacs"
-  dmg_name "Emacs-24.2-universal-10.6.8"
   source "http://emacsformacosx.com/emacs-builds/Emacs-24.3-universal-10.6.8.dmg"
   checksum "92b3a6dd0a32b432f45ea925cfa34834c9ac9f7f0384c38775f6760f1e89365a"
   action :install
