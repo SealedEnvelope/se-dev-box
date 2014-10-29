fc0df5fe1d043258ece76eda0c94aea94f887404
Jonathon Ramsey <jonathon.ramsey@gmail.com>
2014-10-29 20:18:08 +0000

    Install required TeX packages.

---
+++ texpackages.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require 'etc'
+
+texlive = "/usr/local/texlive"
+
+unless File.exist?(texlive)
+  warn "Basictex isn't installed!"
+  abort
+end
+
+if File.stat(texlive).uid == 0
+  system "sudo chown -R #{ENV["USER"]}:admin #{texlive}"
+end
+
+unless system "which -s tlmgr"
+  warn "Couldn't find tlmgr"
+  abort
+end
+
+%w(lastpage rsfs calrsfs siunitx tikz-qtree).each do |pkg|
+  system "tlmgr info #{pkg} | egrep --quiet '^installed: +Yes' || tlmgr install #{pkg}"
+end
