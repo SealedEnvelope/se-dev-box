14f96027ac1617a0ccc375c202c8cfaffa6bbd0d
Jonathon Ramsey <jonathon.ramsey@gmail.com>
2015-12-15 11:38:16 +0000

    syncthing

--- packages.sh
+++ packages.sh
@@ -31,23 +31,23 @@ install_or_check_for_upgrade brew-cask
 install_or_check_for_upgrade bash
 install_or_check_for_upgrade bash-completion
 install_or_check_for_upgrade bash-git-prompt
+install_or_check_for_upgrade chruby
 install_or_check_for_upgrade coreutils
+install_or_check_for_upgrade emacs --cocoa --with-gnutls
 install_or_check_for_upgrade findutils
 install_or_check_for_upgrade gawk
 install_or_check_for_upgrade ghostscript
+install_or_check_for_upgrade git
 brew tap homebrew/dupes 2>/dev/null
 install_or_check_for_upgrade homebrew/dupes/grep
 install_or_check_for_upgrade gnu-sed
-
-install_or_check_for_upgrade chruby
-install_or_check_for_upgrade emacs --cocoa --with-gnutls
-install_or_check_for_upgrade git
 install_or_check_for_upgrade mackup
 install_or_check_for_upgrade mysql
 install_or_check_for_upgrade pandoc
 install_or_check_for_upgrade phantomjs
 install_or_check_for_upgrade ruby-install
 install_or_check_for_upgrade sox --with-lame
+install_or_check_for_upgrade syncthing
 install_or_check_for_upgrade tmux
 install_or_check_for_upgrade w3m
 install_or_check_for_upgrade wget
